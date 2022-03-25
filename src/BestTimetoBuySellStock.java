b981bdae72843fbd4cc05c68b7426dd6799a565a
Daniel Kwang <Danielkwang@outlook.com>
2022-03-25 13:09:57 +0800

    BestTimeToBuyStocks

---
+++ src/BestTimetoBuySellStock.java
@@ -0,0 +1,15 @@
+
+public class BestTimetoBuySellStock {
+
+	public static void main(String[] args) {
+		// 121. Best Time to Buy and Sell Stock
+		
+		
+
+	}
+	
+    public static int maxProfit(int[] prices) {
+        return 0;
+    }
+
+}
